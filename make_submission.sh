6495eeebdde4ca56d94639ac9d9984f14f0ab57c
Robin Brown <robinbrown@Robins-MacBook-Pro.local>
2021-11-16 00:23:05 -0800

    more robust submission

--- make_submission.sh
+++ make_submission.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-zip hw4.zip ekf.py particle_filter.py turtlebot_model.py
+submission_file="submission/HW4"
+hw_dir="HW4"
+mkdir -p "./${submission_file}"
+
+find "./${hw_dir}" -name "ekf.py" -exec cp -rf "{}" "./${submission_file}" \;
+find "./${hw_dir}" -name "particle_filter.py" -exec cp -rf "{}" "./${submission_file}" \;
+find "./${hw_dir}" -name "turtlebot_model.py" -exec cp -rf "{}" "./${submission_file}" \;
+
+cd "./submission"
+zip -r hw4.zip HW4
+cd ..
+mv "./submission/hw4.zip" .
+# zip -r hw4.zip "./${submission_file}"
